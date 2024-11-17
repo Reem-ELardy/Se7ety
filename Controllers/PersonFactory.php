e0294b8ad0348c3a3eedd46f2da0cc0532d58ff9
GannaMohamed32 <49179701+gana22@users.noreply.github.com>
2024-11-17 20:01:57 +0200

    update Address part in SignUp factory

--- Controllers/PersonFactory.php
+++ Controllers/PersonFactory.php
@@ -34,29 +34,29 @@ class PersonFactory{
         else{
             return null;
         }
+        
     }
-
-    public function SignupFactory($name, $age, $email, $password, $role) {
+    public function SignupFactory($name, $age, $email, $password, $role,$AddressID) {
         $user = null;
         $result = null;
         print("\nI am here2");
         switch ($role) {
             case 'Volunteer':
                 print("\nDoner");
-                $user = new Volunteer(name:$name, age:$age, email:$email, password:$password);
+                $user = new Volunteer(name:$name, age:$age, email:$email, password:$password,addressId:$AddressID);
                 print("\nConstructor Done");
                 $result = $user->signup($name, $age, $password, $email);
                 print("\nDone");
                 break;
             case 'Donor':
                 print("\nDoner");
-                $user = new Donor(name:$name, age:$age, email:$email, password:$password);
+                $user = new Donor(name:$name, age:$age, email:$email, password:$password,addressId:$AddressID);
                 print("\nDoner Constructor Done");
                 $result = $user->signup($name, $age, $password, $email);
                 print("\nDone");
                 break;
             case 'Patient':
-                $user = new Patient(name:$name, age:$age, email:$email, password:$password);
+                $user = new Patient(name:$name, age:$age, email:$email, password:$password,addressId:$AddressID);
                 print("\nDoner Constructor Done");
                 $result = $user->signup($name, $age, $password, $email);
                 
@@ -72,6 +72,5 @@ class PersonFactory{
         }
         
     }
-   
 }
 ?>
