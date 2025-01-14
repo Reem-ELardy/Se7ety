a4da73ac22db8ebc87b6e135dc9dc985508f935e
salmasherif846 <155260472+salmasherif846@users.noreply.github.com>
2025-01-14 21:02:45 +0200

    Update DB.php

--- DB-creation/DB.php
+++ DB-creation/DB.php
@@ -100,7 +100,7 @@ run_queries_create_DB(
             Quantity INT,
             IsDeleted tinyint(1) NOT NULL DEFAULT 0
         )",
-        "CREATE TABLE IF NOT EXISTS Admin (
+        "CREATE TABLE IF NOT EXISTS $dbname.Admin (
             ID INT PRIMARY KEY AUTO_INCREMENT,
             PersonID INT NOT NULL,
             Role ENUM('DonationAdmin', 'PaymentAdmin') NOT NULL,
