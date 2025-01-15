1a61eb825892caf96b81872f93ae6dd5f73d1ec0
hannaussama <149675820+hannaussama@users.noreply.github.com>
2025-01-15 04:23:48 +0200

    Handling SMS-Comm

--- Models/SMSComm.php
+++ Models/SMSComm.php
@@ -4,11 +4,11 @@ class SMS implements ICommunicationStrategy{
 
     function send_communication(string $message, Person $person, Subject $event) {
         echo "Sending Email to {$person->getName()}: $message\n";
-            $recipientPhone = $person->getPhoneNumber(); 
+            $recipientPhone = $person->getPhone(); 
             if (!$recipientPhone) {
                 return false;
             }  
-        $communication = new Communication($this , $message ,$event, $person, MessageType::Email);
+        $communication = new Communication($this , $message ,$event, $person, MessageType::SMS);
         if ($communication->createCommunication()) {
             return true;
         } else {
